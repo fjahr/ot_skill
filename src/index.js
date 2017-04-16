5247b607cf81ef9ae27a5214eb63c34a5690a0d7
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-16 23:23:58 +0200

    OT api requests are included in responses

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 "use strict";
 exports.__esModule = true;
 var Alexa = require("alexa-sdk");
+var Request = require("request");
 var handlers = {
     "SynonymsIntent": function () {
         var self = this;
         var intentRequest = self.event.request;
         var value = intentRequest.intent.slots.word.value;
-        // Insert OT api call
-        var speechOutput = "Synonyme für " + value + " sind foo, bar, baz.";
-        self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+        var url = 'https://www.openthesaurus.de/synonyme/search?q=' + value + '&format=application/json';
+        Request(url, function (error, response, body) {
+            if (error) {
+                var speechOutput = "Leider gab es einen Fehler. Bitte versuchen sie es in wenigen Minuten erneut.";
+                self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+            }
+            else {
+                var json = JSON.parse(body);
+                var terms = json['synsets'][0].terms;
+                var syns = [];
+                for (var _i = 0, terms_1 = terms; _i < terms_1.length; _i++) {
+                    var term = terms_1[_i];
+                    syns.push(term["term"]);
+                }
+                var speechOutput = "Synonyme für " + value + " sind " + syns.join(", ") + ".";
+                self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+            }
+            ;
+        });
     }
 };
 var Handler = (function () {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import * as Alexa from "alexa-sdk";
+import * as Request from 'request';
 
 let handlers: Alexa.Handlers = {
   "SynonymsIntent": function() {
     let self: Alexa.Handler = this;
     let intentRequest = <Alexa.IntentRequest> self.event.request;
     let value = intentRequest.intent.slots.word.value;
+    let url = 'https://www.openthesaurus.de/synonyme/search?q=' + value + '&format=application/json'
 
-    // Insert OT api call
+    Request(url, function(error, response, body) {
+      if (error) {
+        let speechOutput = "Leider gab es einen Fehler. Bitte versuchen sie es in wenigen Minuten erneut.";
+        self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+      } else {
+        let json = JSON.parse(body);
+        let terms = json['synsets'][0].terms
+        var syns = []
 
-    let speechOutput = "Synonyme für " + value + " sind foo, bar, baz.";
-    self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+        for(let term of terms) {
+          syns.push(term["term"])
+        }
+
+        let speechOutput = "Synonyme für " + value + " sind " + syns.join(", ") + ".";
+        self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+      };
+    });
   }
 }
 

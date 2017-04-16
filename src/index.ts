1ac23d658bcec9b51a0621e8a74f2f24516db871
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-16 17:17:19 +0200

    add first intent handler draft

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import * as Alexa from "alexa-sdk";
 
 let handlers: Alexa.Handlers = {
+  "SynonymsIntent": function() {
+    let self: Alexa.Handler = this;
+    let intentRequest = <Alexa.IntentRequest> self.event.request;
+    let value = intentRequest.intent.slots.word.value;
 
+    // Insert OT api call
+
+    let speechOutput = "Synonyme für " + value + " sind foo, bar, baz.";
+    self.emit(":tellWithCard", speechOutput, "Open Thesaurus", speechOutput);
+  }
 }
 
 export class Handler {
